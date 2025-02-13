d00a99be1b6677e40d13aefcfd138a7b938bdd9e
SuelenCbrl <suelen.cbrl.sc@gmail.com>
2025-02-13 11:40:09 -0400

    atualização

--- app/views/areaPrivada.php
+++ app/views/areaPrivada.php
@@ -24,46 +24,49 @@ $usuarios = $usuario->listarUsuarios();
         <div class="texto"><p class="title">Lista de Clientes</p></div>
             <div class="area-tabela">
                 <div class="card">
-                        <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Nome</th>
-                                <th>Telefone</th>
-                                <th>Email</th>
-                                <th>Edição</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if (count($usuarios) > 0): ?>
-                                <?php foreach ($usuarios as $u): ?>
-                                    <tr>
-                                        <td><?php echo $u['id_usuario']; ?></td>
-                                        <td><?php echo $u['nome']; ?></td>
-                                        <td><?php echo $u['telefone']; ?></td>
-                                        <td><?php echo $u['email']; ?></td>
-                                        <td>
-                                            <div class="botoes-acoes">
-                                                <a class="LetEdit" href="../../app/classes/usuarios/editar.php?id=<?php echo $u['id_usuario']; ?>">Editar</a>
-                                                <a class="LetExcluir" href="../../app/classes/usuarios/excluir.php?id=<?php echo $u['id_usuario']; ?>" onclick="return confirm('Tem certeza que deseja excluir este usuário?');">Excluir</a>
-                                            </div>
-                                        </td>
+                    <div id="tela-branca"> 
+                        <div class="tabela-responsiva">
+                                    <table>
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nome</th>
+                                            <th>Telefone</th>
+                                            <th>Email</th>
+                                            <th>Edição</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php if (count($usuarios) > 0): ?>
+                                            <?php foreach ($usuarios as $u): ?>
+                                                <tr>
+                                                    <td><?php echo $u['id_usuario']; ?></td>
+                                                    <td><?php echo $u['nome']; ?></td>
+                                                    <td><?php echo $u['telefone']; ?></td>
+                                                    <td><?php echo $u['email']; ?></td>
+                                                    <td>
+                                                        <div class="botoes-acoes">
+                                                            <a class="LetEdit" href="../../app/classes/usuarios/editar.php?id=<?php echo $u['id_usuario']; ?>">Editar</a>
+                                                            <a class="LetExcluir" href="../../app/classes/usuarios/excluir.php?id=<?php echo $u['id_usuario']; ?>" onclick="return confirm('Tem certeza que deseja excluir este usuário?');">Excluir</a>
+                                                        </div>
+                                                    </td>
 
-                                        </td>
-                                    </tr>
-                                    
-                                <?php endforeach; ?>
-                            <?php else: ?>
-                                <tr>
-                                    <td colspan="4" style="text-align: center;">Nenhum usuário cadastrado.</td>
-                                </tr>
-                            <?php endif; ?>
-                        </tbody>
-                    </table>
-                        <div class="area-Bvoltar">
-                            <button class="bVoltar" onclick="window.location.href='index.php'">Sair</button>
-                        </div>
-
+                                                    </td>
+                                                </tr>
+                                                
+                                            <?php endforeach; ?>
+                                        <?php else: ?>
+                                            <tr>
+                                                <td colspan="4" style="text-align: center;">Nenhum usuário cadastrado.</td>
+                                            </tr>
+                                        <?php endif; ?>
+                                    </tbody>
+                                </table>
+                                <div class="area-Bvoltar">
+                                    <button class="bVoltar" onclick="window.location.href='index.php'">Sair</button>
+                                </div>
+                            </div>
+                    </div>                            
                 </div>
             </div>
         </div>
